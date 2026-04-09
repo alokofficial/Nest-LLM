1e6db51639b56e12f0058aa3d845ad80f999a401
alokofficial <aloknh31@gmail.com>
2026-04-09 11:51:56 +0530

    auth app intial setup plus user data request created.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './dto/registerUser.dto';
+import bcrypt from 'bcrypt';
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService:UserService){}
+  async registerUser(registerUserDto:RegisterDto){ 
+    const hashPassword = await bcrypt.hash(registerUserDto.password, 10)
+    return this.userService.createUser({...registerUserDto, password:hashPassword});
+  }
+}
